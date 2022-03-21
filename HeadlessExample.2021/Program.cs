d114d81b3a5dc3ee63107d58956fc84b37ee4921
Jorge Costa <jorge.costa@trimble.com>
2022-03-21 17:59:05 +0200

    upgrade headless project for TS 2021 and above

---
+++ HeadlessExample.2021/Program.cs
@@ -0,0 +1,81 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Copyright © 2022 Trimble Solutions Corporation. Trimble Solutions Corporation is a Trimble Company">
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace HeadlessExample
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    using Tekla.Structures.Model;
+    using Tekla.Structures.Service;
+
+    class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args">additional version field: Ex: HeadlessExample.2021.exe 2021.0 for running example against 2021 version of ts</param>
+        static void Main(string[] args)
+        {
+            // default ts to use. change to 2021 as example. 
+            var tsVersion = "2022.0";
+            if (args.Length != 0)
+            {
+                tsVersion = args[0];
+            }
+
+            var binDir = $@"C:\Program Files\Tekla Structures\{tsVersion}\bin";
+            if (tsVersion.Equals("2021.0"))
+            {
+                // backwards compitbility
+                binDir = $@"C:\Program Files\Tekla Structures\{tsVersion}\nt\bin";
+            }
+
+            AppDomain.CurrentDomain.AssemblyResolve += (s, a) => CurrentDomainAssemblyResolve(a, binDir);
+            // TeklaStructuresService needs to know the location of the TS binaries.
+            // additionally we need to set a resolver to load dlls from binary folder
+            using (var service = new TeklaStructuresService(
+                new DirectoryInfo(binDir),
+                "ENGLISH",
+                new FileInfo($@"C:\ProgramData\Trimble\Tekla Structures\{tsVersion}\Environments\default\env_Default_environment.ini"),
+                new FileInfo($@"C:\ProgramData\Trimble\Tekla Structures\{tsVersion}\Environments\default\role_Steel_Detailer.ini")))
+            {
+                // change for model path
+                var modelPath = @"C:\TeklaStructuresModels\New model";
+                // see Initialize for role, license and identity
+                service.Initialize(new DirectoryInfo(modelPath));
+
+                //!IMPORTANT NOTICE!
+                //!IMPORTANT NOTICE!
+                //!IMPORTANT NOTICE!
+                //You cannot run the above code when in debugger. 
+                //You can add a ReadKey here, so you can attach debugger to be able to debug your own code.
+                //Console.ReadKey();
+
+                // at this point we can call TeklaOpen api to do TS operations
+                Console.WriteLine("ProjectInfo.Name        : " + new Model().GetProjectInfo().Name);
+                Console.WriteLine("ProjectInfo.Description : " + new Model().GetProjectInfo().Description);
+            } // service must be disposed so that a clean exit is done. Once disposed, it cannot be used again during the process lifetime.
+        }
+
+        /// <summary>
+        /// assembly resolver - for loading dlls from bin without copying dlls into exectution path
+        /// </summary>
+        /// <param name="args">event args</param>
+        /// <param name="binDir">ts binary folder</param>
+        /// <returns>assembly to load</returns>
+        private static System.Reflection.Assembly CurrentDomainAssemblyResolve(ResolveEventArgs args, string binDir)
+        {
+            var requestedAssembly = new AssemblyName(args.Name);
+
+            if (File.Exists(Path.Combine(binDir, requestedAssembly.Name + ".dll")))
+            {
+                return System.Reflection.Assembly.LoadFile(Path.Combine(binDir, requestedAssembly.Name + ".dll"));
+            }
+
+            return null;            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HeadlessTeklaStructuresExample
 This example shows how to use Headless Tekla Structures to interact with model without opening TS UI.
+
+## Licensing
+Currently on Flex is supported. Start TS with flex, close TS and Headless applications can be run.
